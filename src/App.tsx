b5d474ee6cdefd3fa9725cb85ecfa8b3909e22b9
Luka Ku <lukaszkuc93@gmail.com>
2022-02-17 17:52:19 +0100

    display cards in app - to check the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { SingleCartType } from "./components/CardType";
 import "./App.css";
+import { createdMemoryBoard } from "./components/CardType";
 
 function App() {
-    return <div className="App"></div>;
+    const [cards, setCards] = useState<SingleCartType[]>(createdMemoryBoard());
+    console.log(cards);
+    return (
+        <div className="App">
+            {cards.map((card) => {
+                return <p key={card.id}>{card.id}</p>;
+            })}
+        </div>
+    );
 }
 
 export default App;
