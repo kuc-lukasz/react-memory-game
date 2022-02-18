e90aa4a4268a9c9941b63413e0726b5f459bcda5
Luka Ku <lukaszkuc93@gmail.com>
2022-02-18 08:52:47 +0100

    display cards in dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import { SingleCartType } from "./components/CardType";
 import "./App.css";
 import { createdMemoryBoard } from "./components/CardType";
+import { Card } from "./components/Card";
 
 function App() {
     const [cards, setCards] = useState<SingleCartType[]>(createdMemoryBoard());
     console.log(cards);
+    const handleClick = () => {
+        console.log("clicked");
+    };
     return (
         <div className="App">
             {cards.map((card) => {
-                return <p key={card.id}>{card.id}</p>;
+                return (
+                    <Card key={card.id} card={card} callback={handleClick} />
+                );
             })}
         </div>
     );
